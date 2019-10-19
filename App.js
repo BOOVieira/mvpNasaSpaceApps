f306efe361a6bbbdda8bf48adf615ba20410f7f6
Barreira <barreira266@hotmail.com>
2019-10-19 03:52:02 -0300

    Componentes customizados && moment ( parse de datas)

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import {Text, View} from 'react-native';
+import { Text, View } from 'react-native';
+import Botao from './src/Componentes/Inicio';
 
 function App() {
   return (
---
+++ src/Componentes/Inicio.js
@@ -0,0 +1,10 @@
+import styled from 'styled-components/native'
+
+export const Botao = styled.TouchableOpacity`
+    margin: 24;
+    fontSize: 18;
+    fontWeight: bold;
+    textAlign: center;
+    color: #34495e;
+`;  
+
